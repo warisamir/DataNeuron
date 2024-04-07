ad51fb829cb24d2428fd36e73bf687ea0b9f707f
WarisAmir <warisamir1918@gmail.com>
2024-04-08 00:31:32 +0530

    Update FirstComponent.tsx

--- client/src/components/home/FirstComponent.tsx
+++ client/src/components/home/FirstComponent.tsx
@@ -1,6 +1,11 @@
 const FirstComponent = () => {
   return <div>
-    <img src="https://plus.unsplash.com/premium_photo-1670590820921-28e2a8b1150a?q=80&w=2018&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="simple"/>
+    <img src="https://plus.unsplash.com/premium_photo-1670590820921-28e2a8b1150a?q=80&w=2018&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+       style={{objectFit:"cover",
+     width:"30em",
+     height:"30em"
+    }}} 
+      alt="simple"/>
   </div>;
 };
 
